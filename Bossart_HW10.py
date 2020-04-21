bb8037b417457c81d1554f57e88163e53643b983
Holly Bossart <hollybossart@u.boisestate.edu>
2020-04-21 13:07:38 -0600

    finished 1 in .py, need to move over to .ipynb

--- Bossart_HW10.py
+++ Bossart_HW10.py
@@ -15,6 +15,7 @@ mu2 = 10
 sig = 0.5
 delta1 = 0.7          # known delta for this problem
 n = 200
+burn = 200
 
 # part a
 dist1 = np.random.normal(loc=mu1,
@@ -75,30 +76,45 @@ for i in range(m):
 
 
 # plotting for b
+deltas = deltas[burn:]         # burn in period
 plt.figure()
 sns.distplot(deltas,
              bins=np.linspace(0.5, 0.85, 30),
-             kde=True,
-             hist=False,
-             label = 'Fitted KDE') 
+             kde=False,
+             norm_hist=True,
+             label = 'Posterior sample') 
 
-plt.hist(deltas, 
-         density=True, 
-         color='green',
-         bins=np.linspace(0.5, 0.85, 30),
-         rwidth=0.8,
-         label='Posterior sampling of $\delta$')
+# plt.hist(deltas, 
+#          density=True, 
+#          color='green',
+#          bins=np.linspace(0.5, 0.85, 30),
+#          rwidth=0.8,
+#          label='Posterior sampling of $\delta$')
  
 plt.xlabel('$\delta$ value')
 plt.ylabel('Frequency')
-plt.title('Sampling Distribution of $\delta$')
-plt.legend()                                 
+plt.title('Sampling Distribution of $\delta$ using MCMC')
+plt.legend()     
+
+
+# path plot
+xax = np.arange(200, m)
+plt.figure()
+plt.plot(xax, deltas, linewidth=1)
+plt.title('Markov Chain path of $\delta$ with burn-in period')
+plt.xlabel('Iteration')
+plt.ylabel('Value')
+
+                         
 
 b_mean = np.mean(deltas)
 b_var = np.var(deltas)
 print('The sample mean of delta using Markov Chain Monte Carlo is ' + str(b_mean))
 print('The sample variance of delta using Markov Chain Monte Carlo is ' + str(b_var))
-print('using ' + str(m) + ' iterations.')
+print('using ' + str(m) + ' iterations')
+print('and a burn-in period of ' + str(burn))
+
+
 
 
 # part c (random walk chain)
@@ -123,31 +139,34 @@ for i in range(m):
         delta = delta
         
     deltas[i] = delta
-
+    
+    
+deltas = deltas[burn:]
 plt.figure()
 sns.distplot(deltas,
              bins=np.linspace(0.5, 0.85, 30),
-             kde=True,
-             hist=False,
-             label = 'Fitted KDE') 
+             norm_hist=True,
+             kde=False,
+             label = 'Posterior sampling of $\delta$') 
 
-plt.hist(deltas, 
-         density=True, 
-         color='green',
-         bins=np.linspace(0.5, 0.85, 30),
-         rwidth=0.8,
-         label='Posterior sampling of $\delta$')
- 
 plt.xlabel('$\delta$ value')
 plt.ylabel('Frequency')
 plt.title('Sampling Distribution of $\delta$ using Random Walk')
-plt.legend()                                 
+plt.legend()      
+
+# path plot
+plt.figure()
+plt.plot(xax, deltas, linewidth=1)
+plt.title('Random Walk path of $\delta$ with burn-in period')
+plt.xlabel('Iteration')
+plt.ylabel('Value')                           
 
 c_mean = np.mean(deltas)
 c_var = np.var(deltas)
 print('The sample mean of delta using Markov Chain Monte Carlo with random walk is ' + str(c_mean))
 print('The sample variance of delta using Markov Chain Monte Carlo with random walk is ' + str(c_var))
-print('using ' + str(m) + ' iterations.')
+print('using ' + str(m) + ' iterations')
+print('and a burn-in period of ' + str(burn))
 
 
 # part d (logit reparameterization)
@@ -155,14 +174,14 @@ m=100000
 deltas = np.zeros(m)          # this is where we will place our updated deltas
 delta = delta_0 = logit(0.5)  # some value between 0 and 1
 
-def mix2():
+def mix2(vals, delta):
+    return (expit(delta)*stats.norm.pdf(vals, loc=mu1, scale=sig) + (1-expit(delta))*stats.norm.pdf(vals, loc=mu2, scale=sig))
     
-
 def jacob(delta):
-    return np.abs(1/(np.exp(delta)+1)-1/(np.exp(delta)+1)**2)
+    return ((1/(np.exp(delta)+1)-1/(np.exp(delta)+1)**2))
 
-def like2(delta):
-    return np.sum()
+def likelihood2(delta):
+    return np.prod(mix2(mix_vals, delta)) 
 
 for i in range(m):
     
@@ -170,20 +189,43 @@ for i in range(m):
     delt_star = delta + np.random.uniform(-1,1)
     
     # compute Metropolis-Hastings ratio R
-    R = likelihood(delt_star)/likelihood(delta)
+    R = (likelihood2(delt_star)*np.abs(jacob(delt_star)))/(likelihood2(delta)*np.abs(jacob(delta)))
         
     # sample a value for X(t+1) 
-    rand_val = np.random.uniform(0,1)
+    rand_val = np.random.uniform(0,1)  # probability value
     
-    if rand_val <= np.min((R,1)) and delt_star <= 1 and delt_star >= 0:
+    if rand_val <= np.min((R,1)):
         delta = delt_star
     else:
         delta = delta
         
     deltas[i] = delta
 
+deltas = deltas[burn:]
 plt.figure()
-plt.hist(deltas)   
+sns.distplot(deltas,
+             bins=np.linspace(0.5, 0.85, 30),
+             norm_hist=True,
+             kde=False,
+             label = 'Posterior sampling of $\delta$') 
 
-    
+plt.xlabel('$\delta$ value')
+plt.ylabel('Frequency')
+plt.title('Sampling Distribution of $\delta$ using Random Walk')
+plt.legend()      
+
+# path plot
+plt.figure()
+plt.plot(xax, deltas, linewidth=1)
+plt.title('Random Walk Reparameterization path of $\delta$ with burn-in period')
+plt.xlabel('Iteration')
+plt.ylabel('Value')                           
+
+d_mean = np.mean(deltas)
+d_var = np.var(deltas)
+print('The sample mean of delta using Markov Chain Monte Carlo with reparameterized random walk is ' + str(d_mean))
+print('The sample variance of delta using Markov Chain Monte Carlo with reparameterized random walk is ' + str(d_var))
+print('using ' + str(m) + ' iterations')
+print('and a burn-in period of ' + str(burn))  
+
     
